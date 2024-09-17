dcdead0a0853dd7fcaa7e0528984177b3bb748b8
P. K. Tharindu <pktharindu@outlook.com>
2024-09-17 16:27:38 +0530

    move classes to figure

--- resources/views/flexipics.blade.php
+++ resources/views/flexipics.blade.php
@@ -1,4 +1,4 @@
-<figure>
+<figure @isset($image->class) class="{{ $image->class }}" @endisset>
     <picture>
         @foreach ($sources as $source)
             <source type="{{ $source->type }}"
@@ -10,7 +10,6 @@
 
         <img src="{{ $image->src }}"
              @isset($image->alt) alt="{{ $image->alt }}" @endisset
-             @isset($image->class) class="{{ $image->class }}" @endisset
              @isset($image->loading) loading="{{ $image->loading }}" @endisset
              @isset($image->width) width="{{ $image->width }}" @endisset
              @isset($image->height) height="{{ $image->height }}" @endisset
